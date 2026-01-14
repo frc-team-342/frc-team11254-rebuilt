0e7bf0092b02a4202dbab97fd2edc120ee27fda3
potatoman5951 <kevinyu.110510@gmail.com>
2026-01-14 18:10:28 -0500

    Finished Drive Skeleton Code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,4 +1,4 @@
-// Copyright (c) FIRST and other WPILib contributors.
+  // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
@@ -8,9 +8,13 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import com.revrobotics.spark.SparkMax;
+import frc.robot.subsystems.TankDrive;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -19,6 +23,10 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
+  private TankDrive drive;
+  private XboxController driver;
+  private Command driveWithJoystick;
+
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
@@ -28,8 +36,14 @@ public class RobotContainer {
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    drive = new TankDrive();
+    driver = new XboxController(0);
+    driveWithJoystick = Commands.run(() -> drive.drive(driver));
+
     // Configure the trigger bindings
     configureBindings();
+
+    drive.setDefaultCommand(driveWithJoystick);
   }
 
   /**
--- src/main/java/frc/robot/subsystems/TankDrive.java
+++ src/main/java/frc/robot/subsystems/TankDrive.java
@@ -4,44 +4,68 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.OperatorConstants;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.XboxController;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.PersistMode;
 import com.revrobotics.spark.SparkMax;
-import static frc.robot.Constants.DriveConstants;
+import static frc.robot.Constants.DriveConstants.*;
 
-/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
-public class TankDrive extends Command {
-  private SparkMax frontLeft;
-  private SparkMax frontRight;
-  private SparkMax backLeft;
-  private SparkMax backRight;
-  
+public class TankDrive extends SubsystemBase {
+    private SparkMax frontLeft;
+    private SparkMax frontRight;
+    private SparkMax backLeft;
+    private SparkMax backRight;
+    private DifferentialDrive tankDrive;
+    private SparkMaxConfig frontLeftConfig;
+    private SparkMaxConfig frontRightConfig;
+    private SparkMaxConfig backLeftConfig;
+    private SparkMaxConfig backRightConfig;
   /** Creates a new TankDrive. */
   public TankDrive() {
-    // Use addRequirements() here to declare subsystem dependencies.
+    frontLeft = new SparkMax(FRONT_LEFT_MOTOR, MotorType.kBrushless);
+    frontRight = new SparkMax(FRONT_RIGHT_MOTOR, MotorType.kBrushless);
+    backLeft = new SparkMax(BACK_LEFT_MOTOR, MotorType.kBrushless);
+    backRight = new SparkMax(BACK_RIGHT_MOTOR, MotorType.kBrushless);
+    frontLeftConfig = new SparkMaxConfig();
+    frontRightConfig = new SparkMaxConfig();
+    backLeftConfig = new SparkMaxConfig();
+    backRightConfig = new SparkMaxConfig();
+    tankDrive = new DifferentialDrive(frontLeft, frontRight);
+      
+
+    frontLeftConfig
+      .idleMode(IdleMode.kBrake)
+      .smartCurrentLimit(60)
+      .inverted(true);
+
+    frontRightConfig
+      .idleMode(IdleMode.kBrake)
+      .smartCurrentLimit(60);
+
+    backLeftConfig.apply(frontLeftConfig).follow(FRONT_LEFT_MOTOR);
+    backRightConfig.apply(frontRightConfig).follow(FRONT_RIGHT_MOTOR);
+
+    frontLeft.configure(frontLeftConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    frontRight.configure(frontRightConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    backLeft.configure(backLeftConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    backRight.configure(backRightConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {}
+  public void drive(XboxController driver){
+    tankDrive.arcadeDrive(MathUtil.applyDeadband(driver.getLeftY(), 0.05), MathUtil.applyDeadband(driver.getRightY(), 0.05));
+  }
 
-  // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
+  public void periodic() {
+    // This method will be called once per scheduler run
   }
 }
